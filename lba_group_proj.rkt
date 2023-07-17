09af2b1a0ed2a18853020ffb1d9b8f514fbc82e6
teeoladipo <132300657+teeoladipo@users.noreply.github.com>
2023-07-17 16:49:33 -0400

    Update lba_group_proj.rkt

--- lba_group_proj.rkt
+++ lba_group_proj.rkt
@@ -1,28 +1,32 @@
 #lang racket
+
 (require rml-neural/activation)
 
+(require plot)
 ;; data hash table from article, table 1; experiment 5
-(define model-param-expt5 (hash 't0 0.438
-                                'a 460
-                                'b 460
-                                's 0.422
-                                'vp 1.07
-                                'vr 1.0
-                                'vh 1.36
-                                'vl 1.00
-                                'vv 0.802))
+(define model-param-expt5 (hash 't0 0.438 ;; non-decision response time
+                                'a 460 ;; upper end of start point distribution
+                                'b 460 ;; response threshold
+                                's 0.422 ;; standard deviation
+                                'vp 1.07 ;; mean drift rate for pseudowords
+                                'vr 1.0 ;; basic parameter. starting small but mean drift rate for random letter strings
+                                'vh 1.36 ;; mean drift rate for high-frequency words
+                                'vl 1.00 ;; mean drift rate for low-frequency words
+                                'vv 0.802)) ;; mean drift rate for very low-frequency words
 
 (define (lba-decision-time param)
   (let* ((t0 (hash-ref param 't0))
          (a (hash-ref param 'a))
          (b (hash-ref param 'b))
          (s (hash-ref param 's))
-         (vr (if (hash-has-key? param 'vr) (hash-ref param 'vr) 1.0)) ;; 'vr is missing from the table. tedious but it searches if the parameter is there but fi not, it uses a ddefault drift rate value of 1.0 that I hard-coded 
-         (drift (gaussian vr s))
-         (rand-k (- b a)))
+         (vr (hash-ref param 'vr)) 
+         (drift (gaussian vr s)) ;; supposed to be a nice visual of a gaussian using the s as an activator 
+         (rand-k (- b a))) ;; response caution? 
     (if (>= drift 0)
-        (/ (- b t0) drift)
+        (/ (- b t0) drift) ;; time for accumulator to reach the threshold
         (/ (- rand-k t0) (- drift)))))
                   
 (define decision-time (lba-decision-time model-param-expt5))
 (displayln decision-time)
+
+(plot decision-time)
